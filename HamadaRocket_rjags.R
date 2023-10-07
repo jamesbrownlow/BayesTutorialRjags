2947f571050ec6b4ed0121ede2af98befb8618e8
Dr. J <jbrownlow@avc.edu>
2023-10-06 17:21:40 -0700

    Add files via upload

--- HamadaRocket_rjags.R
+++ HamadaRocket_rjags.R
@@ -19,12 +19,13 @@ model {
   # Prior: Uniform prior for binomial probability
   #p ~ dunif(0, 1) # vague prior
   #p ~ dbeta(0.5, 0.5) #Jeffreys prior
-  #p ~ dbeta(2.7, 5.8) # informative prior
-  x ~ dexp(0.1)
-  #logit
-  logit(p) <- x
+  p ~ dbeta(2.7, 5.8) # informative prior
+  #x ~ dexp(0.1)
   
-  #probit
+  #logit   p = x/(x+1)  same as p <- ilogit(x)
+  #logit(p) <- x    
+  
+  #probit  p = inverse normal CDF(x)
   #probit(p) <- x
   
   #loglogistic
@@ -40,11 +41,12 @@ data_list <- list(
 )
 
 # Initialize JAGS model
-jags_model <- jags.model(textConnection(model_code), data = data_list, n.chains = 3)
+jags_model <- jags.model(textConnection(model_code), data = data_list,
+                    n.chains = 3)
 
 # Run the MCMC simulation
 jags_samples <- coda.samples(jags_model, variable.names = "p", 
-                             n.iter = n_iterations, n.burnin = n_burnin, thin = n_thin)
+                    n.iter = n_iterations, n.burnin = n_burnin, thin = n_thin)
 
 # Load the coda package for posterior analysis
 library(coda)
@@ -58,8 +60,18 @@ summary(posterior_samples)
 # Plot the posterior distribution
 plot(posterior_samples, main = "Posterior Distribution of p")
 
+
 kd = density(posterior_samples)
 plot(kd, lwd=3)
+
+posterior.p = as.vector(posterior_samples, mode = 'numeric')
+meanPost = mean(posterior.p)
+modePost = max(kd$y)
+modeProb = kd$x[kd$y==modePost]
+stdPost = sd(posterior.p)
+pAxis = seq(0, 0.8, length.out=50)
+lines(pAxis, dnorm(pAxis, mean=meanPost, sd = stdPost), col='green', lty=2,lwd=3)
+
 
 sortedSamples = sort(posterior_samples)
 nSorted = length(sortedSamples)
@@ -72,7 +84,66 @@ abline(v = c(lCred, uCred), col='orange', lwd=3)
 HPDI = HPDinterval((posterior_samples), p = 0.9)
 print(HPDI)
 abline(v = c(HPDI[1], HPDI[2]), col='red', lty=2, lwd=3)
-legend('topright',c('90% credibility interval', '90% highest prob interval'), 
-        col=c('orange','red'), lty = c(1,2), lwd=c(3,3))
+legend('topright',c('90% credibility interval', '90% highest prob interval',
+                    'posterior density','posterior approximation'), 
+        col=c('orange','red','black','green'), lty = c(1,2,1,2), 
+       lwd=c(3,3,3,3))
 
+grid()
+
+# A posterior predictive distribution accounts for uncertainty about 
+# p, taken from the posterior distribution of p.  
+
+#Posterior predictive distribution is original likelihood times posterior. 
+# It's the predictions on 'more' data, given your current data. 
+# But current data is itself dependent on the prior. So really, 
+# it's predicted 'more' data given you've worked out the posterior.
+
+#To find the posterior,  group multiply the likelihood of data with prior. 
+#Posterior of Binomial with Beta Prior
+
+#predictive probability of a x successes is
+# p(x|data) = integral(likelihood(x|p)*posterior(p|data))
+
+# we do not have a closed form for posterior(p|data) thus
+# approximate it with a 'fitted' distribution or use data product.
+
+#posterior pred dist'n
+# prior is the posterior dist'n of p
+# likelihood is binomial, as usual
+
+postPredModel_code <- "
+model {
+  # Likelihood: Binomial distribution
+  successes ~ dbin(p, trials)
+  
+  # Prior: normal approximation to posterior
+  p ~ dnorm(m, 1/s) # the new prior for predicted posterior
+}
+"
+# data list
+postData_list = list(successes = observed_successes, trials = total_trials,
+                m = meanPost, s = stdPost)
+
+# initialize  the model
+jags_model_Post <- jags.model(textConnection(postPredModel_code), 
+                      data = postData_list, n.chains = 3)
+
+# Run the MCMC simulation
+jags_samples_Post <- coda.samples(jags_model_Post, variable.names = "p", 
+                  n.iter = n_iterations, n.burnin = n_burnin, thin = n_thin)
+
+summary(jags_samples_Post)
+plot(jags_samples_Post)
+
+# Extract the posterior samples
+post_Pred_samples <- as.mcmc(jags_samples_Post[[1]])
+
+kd_PostP = density(post_Pred_samples)
+
+plot(kd, lwd=3, col='red', lty=2)
+lines(kd_PostP, lwd=3, xlab='probability of success', col='black')
+
+legend('topright',c('predictive posterior','posterior'), col=c('black', 'red'),
+       lty=c(1,2), lwd = c(3,3))
 grid()
